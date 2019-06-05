60e506e0edbcac8bc04586b57455348f45310a40
Dennis Patzer <d.patzer@heimrich-hannot.de>
2019-06-05 14:17:36 +0200

    added translation

--- src/Resources/contao/languages/de/tl_location.php
+++ src/Resources/contao/languages/de/tl_location.php
@@ -5,15 +5,19 @@ $lang = &$GLOBALS['TL_LANG']['tl_location'];
 /**
  * Fields
  */
-$lang['tstamp'][0]    = 'Änderungsdatum';
-$lang['title'][0]     = 'Titel';
-$lang['title'][1]     = 'Geben Sie hier bitte den Titel ein.';
-$lang['published'][0] = 'Veröffentlichen';
-$lang['published'][1] = 'Wählen Sie diese Option zum Veröffentlichen.';
-$lang['start'][0]     = 'Anzeigen ab';
-$lang['start'][1]     = 'Ort erst ab diesem Tag auf der Webseite anzeigen.';
-$lang['stop'][0]      = 'Anzeigen bis';
-$lang['stop'][1]      = 'Ort nur bis zu diesem Tag auf der Webseite anzeigen.';
+$lang['tstamp'][0]      = 'Änderungsdatum';
+$lang['title'][0]       = 'Titel';
+$lang['title'][1]       = 'Geben Sie hier bitte den Titel ein.';
+$lang['description'][0] = 'Beschreibung';
+$lang['description'][1] = 'Geben Sie hier bitte eine Beschreibung.';
+$lang['country'][0]     = 'Land';
+$lang['country'][1]     = 'Wählen Sie hier bitte ein Land aus.';
+$lang['published'][0]   = 'Veröffentlichen';
+$lang['published'][1]   = 'Wählen Sie diese Option zum Veröffentlichen.';
+$lang['start'][0]       = 'Anzeigen ab';
+$lang['start'][1]       = 'Ort erst ab diesem Tag auf der Webseite anzeigen.';
+$lang['stop'][0]        = 'Anzeigen bis';
+$lang['stop'][1]        = 'Ort nur bis zu diesem Tag auf der Webseite anzeigen.';
 
 /**
  * Legends

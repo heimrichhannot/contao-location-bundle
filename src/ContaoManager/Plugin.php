7460ed49ff02f28bcea3f0b0e608598a839eccc3
Thomas Körner <t.koerner@heimrich-hannot.de>
2021-10-28 11:03:19 +0200

    refactored to new bundle structure

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,11 +6,9 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\ManagerPlugin\Config\ConfigPluginInterface;
 use HeimrichHannot\LocationBundle\HeimrichHannotLocationBundle;
-use Symfony\Component\Config\Loader\LoaderInterface;
 
-class Plugin implements BundlePluginInterface, ConfigPluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
@@ -24,13 +22,5 @@ class Plugin implements BundlePluginInterface, ConfigPluginInterface
         return [
             BundleConfig::create(HeimrichHannotLocationBundle::class)->setLoadAfter($loadAfter)
         ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig)
-    {
-        $loader->load('@HeimrichHannotLocationBundle/Resources/config/services.yml');
     }
 }
